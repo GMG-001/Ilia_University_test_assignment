8017f8c8720368560ffbb755cb5a5d1909a35d25
giorgi <giorgimegrelishvili45@gmail.com>
2021-10-31 17:35:09 +0400

    add_is_caching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,31 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
 {
-    public function posts()
+    public function posts():object
     {
-        $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
-        $posts = collect($posts);
+         cache()->remember('posts', now()->addMinutes(5), function (){
+            return Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+        });
+
+        $posts = collect(cache('posts'));
         $uniqueUserIds = $posts->unique('userId');
         $countUniquePosts = $posts->countBy('userId');
+
+
 
         return view('posts',compact('uniqueUserIds','countUniquePosts'));
     }
 
-    public function userPosts(int $id)
+    public function userPosts(int $id): object
     {
-        $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+        cache()->remember('posts', now()->addMinutes(5), function (){
+            return Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+        });
+
+        $posts = collect(cache('posts'));
         $posts = collect($posts)->where('userId',$id);
 
         return view('userposts',compact('posts'));
     }
 
-    public function post(int $id)
+    public function post(int $id): object
     {
         $post = Http::get('https://jsonplaceholder.typicode.com/posts/'.''.$id.'');
+
         return view('post',compact('post'));
     }
 }
