5a200f1cf1ddf02febd229d3fc847b26c10b912c
giorgi <giorgimegrelishvili45@gmail.com>
2021-11-01 22:14:50 +0400

    small_markup

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,12 +6,12 @@
         <div class="card-body">
             <ul class="list-group">
                 @foreach($uniqueUserIds as $uniqueUserId)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            <div class="col-md-3">
+                        <li class="list-group-item d-flex  justify-content-around align-items-center">
+                            <div class="col-md-1">
                                 Author Id:{{$uniqueUserId['userId']}}
                             </div>
                             <span class="badge bg-primary rounded-pill">{{$countUniquePosts[$uniqueUserId['userId']]}} posts</span>
-                            <div class="col-md-6">
+                            <div class="col-md-1">
                                 <a href="{{route('userPosts',$uniqueUserId['userId'])}}" class="btn btn-sm-float-right btn-success">See All</a>
                             </div>
                         </li>
